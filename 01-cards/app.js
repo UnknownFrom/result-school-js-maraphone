138f502bb76404a6de69f3495308c4183b2dca84
Pavel <vetlyugaev@yandex.ru>
2022-08-14 18:18:23 +0300

    create drag&drop

--- 01-cards/app.js
+++ 01-cards/app.js
@@ -1,14 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+slidesPlugin(2)
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
+    
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-slide.classList.remove('active')
-    })
-}
